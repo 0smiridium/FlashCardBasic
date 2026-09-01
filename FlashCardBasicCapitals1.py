b581d86ff50f4834ad840c16c8a0485c1c254524
0smiridium <0smiridium@proton.me>
2026-08-31 21:21:13 -0400

    Add files via upload

---
+++ FlashCardBasicCapitals1.py
@@ -0,0 +1,11 @@
+import time
+
+print("What is the capital of France?, You Have 20 seconds to answer.")
+# Wait for 20 seconds
+time.sleep(20) 
+print("Paris is the capital of France.")
+
+print("What is the capital of the USA?, You Have 20 seconds to answer.")
+# Wait for 20 seconds
+time.sleep(20) 
+print("Washington D.C. is the capital of the USA.")
